7b0a4aa4373be7048fb82153387f7bcddf46051e
Ahmad <teezamani@gmail.com>
2023-09-10 23:05:37 +0100

    Updating_EnvironmentFile

--- Program.cs
+++ Program.cs
@@ -10,8 +10,19 @@ builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
 
 //Set the Configuration to map appsettings
-var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).Build();
+//var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).Build();
+//builder.Services.Configure<AppSettings>(configuration.GetSection("AppSettings"));
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+
+var configuration = builder.Configuration
+ .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+ .AddJsonFile($"appsettings.json", optional: false)
+ .AddJsonFile($"appsettings.{port}.json", optional: true)
+ .AddEnvironmentVariables()
+ .Build();
 builder.Services.Configure<AppSettings>(configuration.GetSection("AppSettings"));
+builder.WebHost.UseUrls($"http://*:{port}/");
 
 var app = builder.Build();
 
